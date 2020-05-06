2dbda81b48d862bc424bbd44c282cabbedf7b150
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-05-06 13:45:00 -0700

    changed host in pool to socket

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,7 @@ const {un, pw} = require('./hiddenpool.js');
 const pool = new Pool({
   user: un,
   password: pw,
-  host: 'ec2-54-151-33-195.us-west-1.compute.amazonaws.com',
+  host: '/var/run/postgresql',
   database: 'wzrd',
   port: 5432
 })
