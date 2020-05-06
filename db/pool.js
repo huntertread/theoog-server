5f9b0a4e5f747b08fca86b1bcf0b1d0a9bfcc3cf
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-05-06 13:53:48 -0700

    trying connection string

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,14 @@
 const Pool = require('pg').Pool
 const {un, pw} = require('./hiddenpool.js');
+const connectionString = `postgresql://${un}:${pw}@ec2-54-151-33-195.us-west-1.compute.amazonaws.com:5432/wzrd`
 
 const pool = new Pool({
-  user: un,
-  password: pw,
-  host: '/var/run/postgresql',
-  database: 'wzrd',
-  port: 5432
+  // user: un,
+  // password: pw,
+  // host: '/var/run/postgresql',
+  // database: 'wzrd',
+  // port: 5432
+  connectionString: connectionString
 })
 
 module.exports = pool;
