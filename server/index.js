e98dda31511ab6bd8da6d2ff35c915db790cf396
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-05-11 15:16:35 -0700

    included fs import

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const router = require('./router/router.js');
+const fs = require('fs');
 
 const https = require('https');
 
