eb8107580570fc3bbc34f051b48cebbc793a25fc
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-07-03 15:23:16 -0700

    comment clean up

--- server/router/router.js
+++ server/router/router.js
@@ -2,33 +2,23 @@ const router = require('express').Router();
 const dbcontroller = require('../../db/controllers');
 const passport = require('passport')
 
-// router.route('/getallurls/:id')
-//   .get(dbcontroller.getAllUserUrls)
-router.route('/user/:un/url/:id')
-  .get(dbcontroller.getAllUserUrls)
+router.route('/login')
+  .post(passport.authenticate('local'), dbcontroller.submitLoginForm)
 
-// router.route('/:id')
-//   .get(dbcontroller.getUrl)
-router.route('/url/:id')
-  .get(dbcontroller.getUrl)
-
-// router.route('/getExistingUser/:un')
-//   .get(dbcontroller.getExistingUser)
-router.route('/user/:un')
-  .get(dbcontroller.getExistingUser)
-
-// router.route('/')
-//   .post(dbcontroller.postUrl)
-router.route('/url')
-  .post(dbcontroller.postUrl)
-
-// router.route('/register')
-//   .post(dbcontroller.postNewUser)
 router.route('/user')
   .post(dbcontroller.postNewUser)
 
-router.route('/login')
-  .post(passport.authenticate('local'), dbcontroller.submitLoginForm)
+router.route('/user/:un')
+  .get(dbcontroller.getExistingUser)
+
+router.route('/user/:un/url/:id')
+  .get(dbcontroller.getAllUserUrls)
+
+router.route('/url')
+  .post(dbcontroller.postUrl)
+
+router.route('/url/:id')
+  .get(dbcontroller.getUrl)
 
 // router.route('/logout')
 //   .get(dbcontroller.logout)
